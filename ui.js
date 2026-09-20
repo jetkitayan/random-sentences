ab93ddef330cefd28fecb3cffec00b5a434aabc7
jetkitayan <jetkitayan@gmail.com>
2026-09-20 09:06:55 +0900

    statsボタン修正

--- ui.js
+++ ui.js
@@ -7,13 +7,11 @@ function showStats() {
   tbody.innerHTML = "";
   for (const s of allSentences) {
     const c = shownCounts[s.en] || 0;
-    const yes = yesMarks[s.en] ? "●" : "○";
     const star = starMarks[s.en] ? "★" : "☆";
     tbody.innerHTML += `
       <tr>
         <td>${s.jp}</td>
         <td>${s.en}</td>
-        <td style="text-align:center;">${yes}</td>
         <td style="text-align:center;">${star}</td>
         <td style="text-align:center;">${c}</td>
       </tr>`;
